0b87093ea13ff1d5e02b83ddac3d92d223d10e1b
David <dcg29908@email.vccs.edu>
2025-02-03 00:13:21 -0500

    fixing popup

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-    base: "/Multi-page-site/"
+    base: "/Multi-page-site/",
     build: {
         outDir: "docs",
         rollupOptions: {
